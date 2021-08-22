eff7bd58184e7788e995f469c6ba60aaca64eb5f
Bryan Ortuno <bryan.ortuno@epn.edu.ec>
2021-08-21 19:57:20 -0500

    Steps

--- VyV_Project/tests/features/reporte_incidentes.feature
+++ VyV_Project/tests/features/reporte_incidentes.feature
@@ -1,20 +1,16 @@
 # Created by rivas at 17/8/2021
 # language: es
 
-Característica: Reporte de problemas o incidentes
+@VP-2
+Característica: Reportar problemas
   # Enter feature description here
   Como usuario
-  deseo reportar un problema o incidente sucedido en mi ciudad especificando el lugar, el tipo de incidente y cualquier dato que me permita explicar el acontecimiento sucedido
+  deseo reportar un problema sucedido en mi ciudad especificando el lugar, el tipo de incidente y cualquier dato que me permita explicar el acontecimiento sucedido
   para que las autoridades locales se informe del problema o incidente y puedan tomar acciones al respecto.
 
-  Esquema del escenario: Formulario de reporte de problemas o incidentes
-    Dado que el señor Bryan tiene una sesión activa
-    Cuando en la pantalla Inicio selecciona la opción de Reportar
-    Y especifica lo siguiente: el "sector", "dirección","tipo","evidencia","descripción" del problema o incidente
-    Y selecciona el botón "Enviar"
-    Entonces estos datos deben ser guardados
-    Y el señor Bryan debe visualizar el mensaje "Reporte enviado" en color verde
-    Pero si el formulario está incompleto, el señor Bryan debe visualizar el mensaje "Debe llenar todos los campos" en color rojo
+  Escenario: Reportar un problema
+    Dado que el señor Bryan tiene una sesión activa en la pantalla de Reportar
+    Cuando reporta un problema con: el "sector", "dirección","categoría","evidencia","descripción" del problema
+    Entonces el señor Bryan debe visualizar el mensaje "Reporte enviado" en color verde
+    Pero si el formulario está incompleto, el señor Bryan debe visualizar el mensaje "Debe llenar los campos correctamente" en color rojo
 
-    Ejemplos:
-      |  |  # Enter scenario name here
--- VyV_Project/tests/features/steps/reporte_incidentes.py
+++ VyV_Project/tests/features/steps/reporte_incidentes.py
@@ -1,47 +1,26 @@
+import random
+
 from behave import *
 
 use_step_matcher("re")
 
 
-@step("que el señor Bryan tiene una sesión activa")
+@step("que el señor Bryan tiene una sesión activa en la pantalla de Reportar")
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(u'STEP: Dado que el señor Bryan tiene una sesión activa')
+    usuario = Usuario.object.get(username="Bryan")
+    assert usuario.is_authenticated
 
 
-@step("en la pantalla Inicio selecciona la opción de Reportar")
+@step('reporta un problema con: el "sector", "dirección","categoría","evidencia","descripción" del problema')
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(u'STEP: Cuando en la pantalla Inicio selecciona la opción de Reportar')
-
-
-@step('especifica lo siguiente: el "sector", "dirección","tipo","evidencia","descripción" del problema o incidente')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(
-        u'STEP: Y especifica lo siguiente: el "sector", "dirección","tipo","evidencia","descripción" del problema o incidente')
-
-
-@step('selecciona el botón "Enviar"')
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Y selecciona el botón "Enviar"')
-
-
-@step("estos datos deben ser guardados")
-def step_impl(context):
-    """
-    :type context: behave.runner.Context
-    """
-    raise NotImplementedError(u'STEP: Entonces estos datos deben ser guardados')
+    context.reporte = Reporte(Sector.SUR, faker.address, Categoria.DANOS_EN_ESPACIOS_PUBLICOS, faker.file, faker.lorem)
+    assert isinstance(reporte, Reporte)
 
 
 @step('el señor Bryan debe visualizar el mensaje "Reporte enviado" en color verde')
@@ -49,14 +28,22 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(u'STEP: Y el señor Bryan debe visualizar el mensaje "Reporte enviado" en color verde')
+    if context.reporte.es_completado():
+        context.mensaje = context.reporte.notificar()
+        assert context.mensaje == "Reporte enviado"
+    else:
+        assert True
 
 
 @step(
-    'si el formulario está incompleto, el señor Bryan debe visualizar el mensaje "Debe llenar todos los campos" en color rojo')
+    'si el formulario está incompleto, el señor Bryan debe visualizar el mensaje "Debe llenar los campos correctamente" en color rojo')
 def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    raise NotImplementedError(
-        u'STEP: Pero si el formulario está incompleto, el señor Bryan debe visualizar el mensaje "Debe llenar todos los campos" en color rojo')
+
+    if context.reporte.es_completado():
+        assert True
+    else:
+        mensaje = context.reporte.notificar_campos_incorrectos()
+        assert mensaje == "Debe llenar los campos correctamente"
