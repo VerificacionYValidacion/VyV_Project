6f6881573f20132df4efa15a6c502c6df83b2fee
Bryan Ortuno <bryan.ortuno@epn.edu.ec>
2021-08-26 18:38:54 -0500

    step1 V2

--- VyV_Project/tests/features/steps/reporte_incidentes.py
+++ VyV_Project/tests/features/steps/reporte_incidentes.py
@@ -12,9 +12,9 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    #usuario = Usuario.objects.get(correo_electronico="bryan.galindo@gmail.com")
-    #assert usuario.is_authenticated
-    pass
+    usuario = Usuario.objects.get(correo_electronico="bryan.galindo@gmail.com")
+    assert usuario.is_authenticated
+
 
 
 @step('reporta un problema con: el "sector", "dirección","categoría","evidencia","descripción" del problema')
@@ -23,9 +23,8 @@ def step_impl(context):
     :type context: behave.runner.Context
     """
 
-    pass
-    #context.reporte = Reporte(Sector.SUR, faker.address, Categoria.DANOS_EN_ESPACIOS_PUBLICOS, faker.file, faker.lorem)
-    #assert isinstance(reporte, Reporte)
+    context.reporte = Reporte(Sector.SUR, faker.address, Categoria.DANOS_EN_ESPACIOS_PUBLICOS, faker.file, faker.lorem)
+    assert isinstance(reporte, Reporte)
 
 
 @step('el señor Bryan debe visualizar el mensaje "Reporte enviado" en color verde')
@@ -33,12 +32,11 @@ def step_impl(context):
     """
     :type context: behave.runner.Context
     """
-    #if context.reporte.es_completado():
-    #    context.mensaje = context.reporte.notificar()
-    #    assert context.mensaje == "Reporte enviado"
-    #else:
-    #    assert True
-    pass
+    if context.reporte.es_completado():
+        context.mensaje = context.reporte.notificar()
+        assert context.mensaje == "Reporte enviado"
+    else:
+        assert True
 
 @step(
     'si el formulario está incompleto, el señor Bryan debe visualizar el mensaje "Debe llenar los campos correctamente" en color rojo')
@@ -47,9 +45,8 @@ def step_impl(context):
     :type context: behave.runner.Context
     """
 
-    #if context.reporte.es_completado():
-    #    assert True
-    #else:
-    #    mensaje = context.reporte.notificar_campos_incorrectos()
-    #    assert mensaje == "Debe llenar los campos correctamente"
-    pass
+    if context.reporte.es_completado():
+        assert True
+    else:
+        mensaje = context.reporte.notificar_campos_incorrectos()
+        assert mensaje == "Debe llenar los campos correctamente"
